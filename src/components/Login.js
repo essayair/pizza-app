0c5e0f2bc362baf24db5d5c675b1603d00d804f9
Victor Li <essayair@hotmail.com>
2020-03-21 14:06:32 +1000

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,44 @@
 import React from 'react';
 
-
+import './css/login.scss';
 
 const Login = () => {
 
     return (
             
         <div className="login">
-        
-        </div>
+            <h2>Login Form</h2>
+            <button>Login</button>
 
+
+            <div className="login_form">
+                
+                <form method="post">
+                    <div className="imgcontainer">
+                        <span className="close" title="Close Modal">&times;</span>
+                    </div>
+
+                    <div className="container">
+                        <label for="uname"><b>Username</b></label>
+                        <input type="text" placeholder="Enter Username" name="uname" required />
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required />
+                            
+                        <button type="submit">Login</button>
+                        <label>
+                            <input type="checkbox" checked="checked" name="remember" /> Remember me
+                        </label>
+                    </div>
+
+                        <div className="container" >
+                            <button type="button" className="cancelbtn">Cancel</button>
+                            <span className="psw">Forgot <a href="#">password?</a></span>
+                        </div>
+                </form>
+            </div>
+
+        </div>
     )
 }
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import HomePage from '../homePage/HomePage';
-import BuildPizza from '../buildPizza/BuildPizza';
-import PizzaMenu from '../pizzaMenu/PizzaMenu';
+
 import SideNav from '../components/SideNav';
 import SideCart from '../components/SideCart';
 import TopBar from "../components/TopBar";
+import Login from "../components/Login";
+
+
+import HomePage from '../homePage/HomePage';
+import BuildPizza from '../buildPizza/BuildPizza';
+import PizzaMenu from '../pizzaMenu/PizzaMenu';
 import ShoppingCart from '../shoppingCart/ShoppingCart';
 
 const Routes =() => {
@@ -20,6 +24,8 @@ const Routes =() => {
                 <Route exact path="/shoppingcart" component={ ShoppingCart } />
                 <Route exact path="/sidenav" component={ SideNav } />
                 <Route exact path="/sidecart" component={ SideCart } />
+                <Route exact path="/login" component={ Login } />
+
             </Switch>       
     );
 }
